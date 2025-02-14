fe3dd9a4a62a6bf9436631c346791afce67bdea2
Hailey <hailey.soojung@gmail.com>
2025-02-14 22:33:39 +0900

    add day20 quiz

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -13,15 +13,21 @@ import { Route, Routes } from 'react-router-dom';
 const mockData = [
   {
     id: 1,
-    createdDate: new Date().getTime(),
+    createdDate: new Date('2025-02-14').getTime(),
     emotionId: 1,
     content: '1번 일기 내용',
   },
   {
     id: 2,
-    createdDate: new Date().getTime(),
+    createdDate: new Date('2025-02-13').getTime(),
     emotionId: 2,
     content: '2번 일기 내용',
+  },
+  {
+    id: 3,
+    createdDate: new Date('2025-01-11').getTime(),
+    emotionId: 3,
+    content: '3번 일기 내용',
   },
 ];
 function reducer(state, action) {
@@ -37,8 +43,8 @@ function reducer(state, action) {
   }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
